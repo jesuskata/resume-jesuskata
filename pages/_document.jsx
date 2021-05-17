06214658f4f58dabb32da493966550f942ad7e2f
jesuskata <rzarate.alex@gmail.com>
2021-05-17 16:24:33 -0500

    style: 💄 add roboto font with link tag in _document

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -16,6 +16,8 @@ class MyDocument extends Document {
             integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay"
             crossOrigin="anonymous"
           />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
         </Head>
         <body className="body-class">
           <Main />
